282c283088bc2d5d7cdc093c3f47f49d392b5215
Nicolas <corvalan4@gmail.com>
2018-10-03 09:45:33 -0300

    Proyecto Completo

---
+++ web/Configuracion/js/configuracion.js
@@ -0,0 +1,11 @@
+var direccionComercial = "Franklin 1769 3&#186;B <br/>Capital Federal - CP: 1406";
+var razonSocial = "Nicolas Corvalan";
+var cuit = "20-35324632-2";
+var condicionIVA = "IVA Responsable Inscripto";
+var ingresosBrutos = "20-35324632-2";
+var fechaInicioActividades = "10/2012";
+var logo = ""; //Tiene que estar en la ruta bundles/web/images/;
+var decripcionLogo ="<strong>FACTURA FACIL</strong>";
+
+
+
